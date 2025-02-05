86ed825368f5a1264f7dfec7007cd06547c40062
Akshay <akshaymm0805@gmail.com>
2025-02-05 11:13:40 +0530

    log update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -286,6 +286,7 @@ const updateUser = async(req , res) => {
               code: 200,
             };
           });
+          console.log(update);
         res.status(200).json(update);
     }
     catch (error) {
