4735b2f1cc4f1a0ac6495ebe47669b4dda9949f5
Akshay <akshaymm0805@gmail.com>
2025-02-07 16:18:47 +0530

    status

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -136,4 +136,15 @@ const listOrders = async (req , res) => {
     }
 }
 
-export { placeOrder , verifyOrder , userOrder , listOrders }
+const updateStatus = async (req , res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId , {status: req.body.status});
+        res.status(200).json({ success: true , message: "Status Updated" });
+    } 
+    catch (error) {
+        console.log(error);
+        res.status(500).json({success: false , message: "Error"});
+    }
+}
+
+export { placeOrder , verifyOrder , userOrder , listOrders , updateStatus }
