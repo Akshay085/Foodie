0601e732d2b1ae93c78f8e1f2f17f326cec8d4b3
Udbhav905 <prajapatiudbhav95@gmail.com>
2025-03-06 10:35:28 +0530

    ok

--- Frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ Frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,48 +1,54 @@
-import React,{useContext, useEffect}  from 'react'
-import './ExploreMenu.css'
-
-import { StoreContext } from '../../Context/StoreContext.jsx'
+import React, { useContext } from 'react';
+import './ExploreMenu.css';
+import { StoreContext } from '../../Context/StoreContext.jsx';
 import Loaderfrount from '../MyLottieAnimation/Loaderfrount.jsx';
-const ExploreMenu = ({category,setCategory}) => {
-   const {categorylist} =useContext(StoreContext);
-   
- 
+
+const ExploreMenu = ({ category, setCategory }) => {
+    const { categorylist } = useContext(StoreContext);
+
+    // Show loader while data is loading
     if (!categorylist || categorylist.length === 0) {
-        return <Loaderfrount/>;
-      }
- 
-    if(!category){
-        setCategory((category)=>category="All");
-      }
-      console.log(categorylist);
+        return <Loaderfrount />;
+    }
 
-  return (
-    <section className='explore-menu' id='exploremenu'>
-        
-        <h1>Explore Menu</h1>
-        <p className='explore-menu-text'>Choose From This Menu.</p>
-        <div className="explore-menu-list">
-            <div className="explore-menu-list-all">
-            {categorylist===0? <Loaderfrount />:null} 
-            <img  onClick={()=>setCategory("All")}  className={category=="All"? "active" :null} src="\Images\allfoods.jpg" alt="all image"  />
-            <p>All</p>
+    if (!category) {
+        setCategory(() => "All");
+    }
+
+    console.log(categorylist);
+
+    return (
+        <section className='explore-menu' id='exploremenu'>
+            <h1>Explore Menu</h1>
+            <p className='explore-menu-text'>Choose From This Menu.</p>
+            <div className="explore-menu-list">
+                <div className="explore-menu-list-all">
+                    <img
+                        onClick={() => setCategory("All")}
+                        className={category === "All" ? "active" : null}
+                        src="\Images\allfoods.jpg"
+                        alt="all image"
+                    />
+                    <p>All</p>
+                </div>
+                {categorylist.map((item, i) => (
+                    <div
+                        onClick={() => setCategory(prev => (prev === item.name ? "All" : item.name))}
+                        key={i}
+                        className='explore-menu-list-item'
+                    >
+                        <img
+                            className={category === item.name ? "active" : ""}
+                            src={item.image}
+                            alt="items"
+                        />
+                        <p>{item.name}</p>
+                    </div>
+                ))}
             </div>
-            {     
-           
-                categorylist.map((item,i)=>{   
-                    return (
-                        <div onClick={()=>{setCategory(prev => prev===item.name ? "All" : item.name)}}  key={i} className='explore-menu-list-item'>
-                            {categorylist===0? <Loaderfrount />:null} 
-                            <img  className={category===item.name ? "active" : " "} src={item.image} alt="items" />
-                            <p>{item.name}</p>
-                        </div>
-                    )
-                })
-            }
-        </div>
-        <hr></hr>
-    </section>
-  )
-}
+            <hr />
+        </section>
+    );
+};
 
-export default ExploreMenu
+export default ExploreMenu;
