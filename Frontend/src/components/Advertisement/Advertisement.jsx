532743e671c21e4bb1860967e14963a95a7d94f2
Udbhav905 <prajapatiudbhav95@gmail.com>
2025-03-21 10:02:29 +0530

    add gap between two ads

--- Frontend/src/components/Advertisement/Advertisement.jsx
+++ Frontend/src/components/Advertisement/Advertisement.jsx
@@ -25,24 +25,27 @@ const Advertisement = () => {
         <Box sx={{ maxWidth: "100%", overflow: "hidden", mt: 2 }}>
   <Slider {...settings}>
     {ads.map((ad) => (
-      <Card
-        key={ad.id}
-        sx={{
-          borderRadius: 2,
-          height: { xs: "40vw", sm: "40vw", md: "10vw", lg: "25vw" }, 
-        }}
-      >
-        <CardMedia
-          component="img"
-          image={ad.img}
-          alt={`Advertisement ${ad.id}`}
-          sx={{
-            width: "100%",
-            objectFit: "fill",
-            height: "100%",
-          }}
-        />
-      </Card>
+     <Card
+     key={ad.id}
+     sx={{
+       borderRadius: 2,
+       height: { xs: "40vw", sm: "40vw", md: "10vw", lg: "25vw" },
+       p: 1, 
+     }}
+   >
+     <CardMedia
+       component="img"
+       image={ad.img}
+       alt={`Advertisement ${ad.id}`}
+       sx={{
+         width: "100%",
+         objectFit: "fill",
+         height: "100%",
+         borderRadius: 2, // Ensure the image has some roundness
+       }}
+     />
+   </Card>
+   
     ))}
   </Slider>
 </Box>
